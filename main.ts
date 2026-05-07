639e03119f155d10ce8880dc79d94fb68b20f541
Shelchin <hi@shelchin.com>
2026-05-07 16:41:12 +0800

    fx

--- main.ts
+++ main.ts
@@ -116,6 +116,7 @@ async function handler(req: Request): Promise<Response> {
   const headers = new Headers({
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
   });
 
   if (req.method === "OPTIONS") {
